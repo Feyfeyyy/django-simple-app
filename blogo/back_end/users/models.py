cef622b23b2ee2348cf0af88db81b25fc7737f76
Feyaaz <feyaazc94@gmail.com>
2024-06-05 23:47:36 +0100

    Create Custom User Profile

--- blogo/back_end/users/models.py
+++ blogo/back_end/users/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        """
+        Creates and saves a superuser with the given email and password.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, name, password, **extra_fields)
+
+
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+    FREE = 'FR'
+    PRO = 'PR'
+
+    SUBSCRIPTION_CHOICES = (
+        (FREE, 'Free'),
+        (PRO, 'Pro'),
+    )
+    subscription_type = models.CharField(
+        max_length=2,
+        choices=SUBSCRIPTION_CHOICES,
+        default=FREE,
+    )
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.name
