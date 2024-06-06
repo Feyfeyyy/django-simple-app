9b0ab9c534f3354a55185d9a6cd4c07c57ee45d3
Feyaaz <feyaazc94@gmail.com>
2024-06-06 08:39:33 +0100

    Revamp User In Admin

--- blogo/back_end/users/admin.py
+++ blogo/back_end/users/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "name",
+        "subscription_type",
+    )
+    search_fields = (
+        "email",
+        "name",
+    )
+    list_filter = (
+        "subscription_type",
+    )
